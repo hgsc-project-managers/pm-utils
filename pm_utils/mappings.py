e8bc0e46f193e4e59ff8e76f28421871e698d984
jekwatt <jekwatt@users.noreply.github.com>
2021-03-04 12:37:43 -0600

    Add mappings.py

---
+++ pm_utils/mappings.py
@@ -0,0 +1,19 @@
+"""
+Collection of all study mapping for pm_utils.
+More studies will be added here.
+"""
+
+# map abbrev (from merge_name in input report) to the Study/Cohort name
+STUDY_MAPPING = [
+    ("Legacy", "TOPMed Control"),
+    ("TMCONT", "TOPMed Control"),
+    ("TMHASC", "Harvard SCD"),
+    ("TMCGVC", "Causal Genetic Variants of Cardiomyopathy"),
+    ("TMGCUC", "Genetic Causes of Unexplained Cardiomyopathies"),
+    ("TMREDS", "Sickle Cell Disease REDS III"),
+    ("TMPSCR", "PittSCR"),
+    ("TMPCGC", "PCGC"),
+    ("TMMLOF", "MLOF Hemophilia"),
+    # TODO: update OMG SCD
+]
+
