da8de07de38dc766e943527f788f41fdeefa6fde
jekwatt <jekwatt@users.noreply.github.com>
2021-03-11 13:54:36 -0600

    Update comments

--- pm_utils/report_90x.py
+++ pm_utils/report_90x.py
@@ -74,7 +74,7 @@ def load_merge_report(recent_merge_report):
     rtm.rename(columns=d1, inplace=True)
     # use loc to avoid SettingWithCopyWarning warning message
     rtm_sub = rtm.loc[:, rpt_merge_cols]
-    # extract abbrev from merge_name
+    # parse project abbrev & sample_id from merge_name
     decode  = rtm_sub["merge_name"].apply(decode_merge_name)
     rtm_sub[["collection", "sample_id"]] = pd.DataFrame(
     decode.tolist(), index=rtm_sub.index
@@ -82,7 +82,6 @@ def load_merge_report(recent_merge_report):
     cid = rtm_sub["collection"]
     # sample_id from merge_name
     sid = rtm_sub["sample_id"]
-    # add a column 'collection'
     d2 = defaultdict(lambda: None)
     d2.update(STUDY_MAPPING)
     rtm_sub["collection"] = cid.map(d2)
